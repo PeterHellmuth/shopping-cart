c681d92ed0b7768d1084f9d841dd3810edf64e5c
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-10-20 14:32:13 -0500

    Change store title to home link

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,9 +2,8 @@ import { Link } from "react-router-dom"
 import cartImage from "../media/cart-outline.svg"
 
 function NavBar({totalCartItems}){
-
     return  (<div className="nav-bar">
-                <h1 className="store-title">Stuffmart</h1>
+                <Link to="/" className="nav-link"><h1 className="store-title">Stuffmart</h1></Link>
                 <div className="nav-menu">
                     <Link to="/" className="nav-link">Home</Link>
                     <Link to="/Store/" className="nav-link">Store</Link>
